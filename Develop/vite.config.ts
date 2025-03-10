c3a1b76890918fd3c4c99cbd0306e87fb53bffd5
Martin-rojo <164273478+Martin-rojo@users.noreply.github.com>
2025-03-09 19:31:25 -0500

    Update vite.config.ts

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './',
   plugins: [react()],
+  server: {
+    host: true,
+    port: Number(process.env.PORT) || 3000,
+  },
+  preview: {
+    host: true,
+    port: Number(process.env.PORT) || 3000,
+  }
 });
